e4d4256de0fc26cbdd652ca4c50ba73cc2853183
Caleb224 <caleb.beere@gmail.com>
2025-03-18 15:51:02 -0400

    Add lambda function access

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
 
 const schema = a.schema({
   Todo: a
@@ -12,8 +13,9 @@ const schema = a.schema({
           email: a.string(),
           profileOwner: a.string()
       })
-      .authorization((allow) => allow.ownerDefinedIn("profileOwner"))
-});
+      .authorization(allow => allow.ownerDefinedIn("profileOwner"))
+})
+    .authorization(allow => allow.resource(postConfirmation));
 
 export type Schema = ClientSchema<typeof schema>;
 
