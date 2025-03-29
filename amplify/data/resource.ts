e37f80f2318af499b9a2480b922774173e9184a6
Caleb224 <caleb.beere@gmail.com>
2025-03-29 17:25:13 -0400

    Update onUpload handler

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,26 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { postConfirmation } from "../auth/post-confirmation/resource";
 
+const generationPrompt = "Empty prompt";
+
 const schema = a.schema({
+    generateInsight: a
+        .generation({
+            aiModel: a.ai.model("Llama 3.1 405B Instruct"),
+            systemPrompt: generationPrompt
+        })
+        .arguments({
+            input: a.string()
+        })
+        .returns(
+            a.customType({
+                items: a.customType({
+                    name: a.string(),
+                    category: a.string(),
+                    price: a.string()
+                })
+            })
+        ).authorization((allow) => allow.authenticated()),
     UserProfile: a
       .model({
           email: a.string(),
--- amplify/storage/on-upload-handler.ts
+++ amplify/storage/on-upload-handler.ts
@@ -1,6 +1,31 @@
 import type { S3Handler } from 'aws-lambda';
+import { TextractClient, StartExpenseAnalysisCommand } from "@aws-sdk/client-textract";
+
+const config = {
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+}
+
+const client = new TextractClient(config);
 
 export const handler: S3Handler = async (event) => {
-    const objectKeys = event.Records.map((record) => record.s3.object.key);
-    console.log(`Upload handler invoked for objects [${objectKeys.join(', ')}]`);
+    const objectKey = event.Records.map((record) => record.s3.object.key)[0];
+
+    const input = {
+        DocumentLocation: {
+            S3Object: {
+                Bucket: "cloud-computing-2025",
+                Name: objectKey
+            }
+        },
+        OutputConfig: {
+            S3Bucket: "cloud-computing-2025",
+            S3Prefix: `analysis/${objectKey}`
+        }
+    };
+
+    const command = new StartExpenseAnalysisCommand(input);
+
+    await client.send(command);
 };
